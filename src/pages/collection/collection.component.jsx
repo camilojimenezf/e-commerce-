f64dda007538a0743f054a530be56a9e871f0f39
camilojimenezf <camilob270@gmail.com>
2020-03-26 11:13:45 -0300

    finisih first sprint app

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import './collection.styles.scss';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+    return(
+        <div className="collection-page">
+            <h2 className='title'>{title}</h2>
+            <div className="items">
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
+        </div>
+    )
+};
+
+// ownProps (second argument is by default in mapStateToProps) contains all the props of the component
+const mapStateToProps = (state, ownProps) => ({
+    //in this selector is special because depending on the URL parameter, this is why use a currying
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
+
